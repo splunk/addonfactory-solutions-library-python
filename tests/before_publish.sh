ae45ccf80e0be62a9e1f06d17a56107608eb293d
Peter Wu <pwu@splunk.com>
2020-04-01 17:00:17 -0700

    ITSI-6547: Fix before_publish.sh Master branch handling * Don't append a random "-" suffix * Check the right versioned package

--- tests/before_publish.sh
+++ tests/before_publish.sh
@@ -11,13 +11,13 @@ cred=$(echo $cred | tr -d ' ')
 name=`awk '/^Name: .*$/ {print $2}' solnlib.egg-info/PKG-INFO`
 version=`awk '/^__version__.*$/ {print $3}' solnlib/__init__.py | tr -d "\'"`
 new_version=`echo $version | sed -e 's/-dev./.dev/g'`
-if [[ ! -z "$new_version" ]]
+if [[ ! -z "$version_postfix" ]]
 then
     new_version="${new_version}-${version_postfix}"
 fi
 sed -i s/"${version}"/"${new_version}"/g solnlib/__init__.py
 
-uri=$(curl -Ssu $cred -GET https://repo.splunk.com/artifactory/api/storage/pypi/$name/$version/$name-$version.tar.gz | node -pe "JSON.parse(require('fs').readFileSync('/dev/stdin').toString()).uri")
+uri=$(curl -Ssu $cred -GET https://repo.splunk.com/artifactory/api/storage/pypi/$name/$new_version/$name-$new_version.tar.gz | node -pe "JSON.parse(require('fs').readFileSync('/dev/stdin').toString()).uri")
 if [[ $uri == "undefined" ]]
 then
     echo {\"exists\": false, \"version\":\"${new_version}\"} > upload_check.json
