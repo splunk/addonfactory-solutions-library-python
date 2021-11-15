2eb945b5536b72dd1dd6b0aeff402c2e56c3c443
Artem Rys <arys@splunk.com>
2021-11-15 15:28:25 +0100

    docs: add timer_queue.py to the docs

--- solnlib/timer_queue.py
+++ solnlib/timer_queue.py
@@ -22,6 +22,7 @@ import queue as Queue
 import threading
 import traceback
 from time import time
+from typing import Callable, List, Tuple
 
 import sortedcontainers as sc
 
@@ -29,23 +30,22 @@ __all__ = ["Timer", "TimerQueueStruct", "TimerQueue"]
 
 
 class Timer:
-    """Timer wraps the callback and timestamp related attributes.
-
-    :param callback: Arbitrary callable object.
-    :type callback: ``callable object``
-    :param when: The first expiration time, seconds since epoch.
-    :type when: ``integer``
-    :param interval: Timer interval, if equals 0, one time timer, otherwise
-        the timer will be periodically executed
-    :type interval: ``integer``
-    :param ident: (optional) Timer identity.
-    :type ident:  ``integer``
-    """
+    """Timer wraps the callback and timestamp related attributes."""
 
     _ident = 0
     _lock = threading.Lock()
 
-    def __init__(self, callback, when, interval, ident=None):
+    def __init__(self, callback: Callable, when: int, interval: int, ident: int = None):
+        """
+        Initializes Timer.
+
+        Arguments:
+            callback: Arbitrary callable object.
+            when: The first expiration time, seconds since epoch.
+            interval: Timer interval, if equals 0, one time timer, otherwise
+                the timer will be periodically executed.
+            ident: (optional) Timer identity.
+        """
         self._callback = callback
         self.when = when
         self.interval = interval
@@ -94,32 +94,32 @@ class TimerQueueStruct:
         self._timers = sc.SortedSet()
         self._cancelling_timers = {}
 
-    def add_timer(self, callback, when, interval, ident):
+    def add_timer(
+        self, callback: Callable, when: int, interval: int, ident: int
+    ) -> Timer:
         """Add timer to the data structure.
 
-        :param callback: Arbitrary callable object.
-        :type callback: ``callable object``
-        :param when: The first expiration time, seconds since epoch.
-        :type when: ``integer``
-        :param interval: Timer interval, if equals 0, one time timer, otherwise
-            the timer will be periodically executed
-        :type interval: ``integer``
-        :param ident: (optional) Timer identity.
-        :type ident:  ``integer``
-        :returns: A timer object which should not be manipulated directly by
-            clients. Used to delete/update the timer
-        :rtype: ``solnlib.timer_queue.Timer``
+        Arguments:
+            callback: Arbitrary callable object.
+            when: The first expiration time, seconds since epoch.
+            interval: Timer interval, if equals 0, one time timer, otherwise
+                the timer will be periodically executed
+            ident: (optional) Timer identity.
+
+        Returns:
+            A timer object which should not be manipulated directly by
+                clients. Used to delete/update the timer.
         """
 
         timer = Timer(callback, when, interval, ident)
         self._timers.add(timer)
         return timer
 
-    def remove_timer(self, timer):
+    def remove_timer(self, timer: Timer):
         """Remove timer from data structure.
 
-        :param timer: Timer object which is returned by ``TimerQueueStruct.add_timer``.
-        :type timer: ``Timer``
+        Arguments:
+            timer: Timer object which is returned by ``TimerQueueStruct.add_timer``.
         """
 
         try:
@@ -131,12 +131,11 @@ class TimerQueueStruct:
         else:
             self._cancelling_timers[timer.ident] = timer
 
-    def get_expired_timers(self):
+    def get_expired_timers(self) -> Tuple:
         """Get a list of expired timers.
 
-        :returns: a list of ``Timer``, empty list if there is no expired
-            timers.
-        :rtype: ``list``
+        Returns:
+            A tuple of ``Timer``, empty list if there is no expired timers.
         """
 
         next_expired_time = 0
@@ -151,21 +150,23 @@ class TimerQueueStruct:
 
         if self._timers:
             next_expired_time = self._timers[0].when
-        return (next_expired_time, expired_timers)
+        return next_expired_time, expired_timers
 
-    def reset_timers(self, expired_timers):
+    def reset_timers(self, expired_timers: List[Timer]) -> bool:
         """Re-add the expired periodical timers to data structure for next
         round scheduling.
 
-        :returns: True if there are timers added, False otherwise.
-        :rtype: ``bool``
+        Arguments:
+            expired_timers: List of expired timers.
+
+        Returns:
+            True if there are timers added, False otherwise.
         """
 
         has_new_timer = False
         cancelling_timers = self._cancelling_timers
         for timer in expired_timers:
             if timer.ident in cancelling_timers:
-                logging.INFO("Timer=%s has been cancelled", timer.ident)
                 continue
             elif timer.interval:
                 # Repeated timer
@@ -175,11 +176,11 @@ class TimerQueueStruct:
         cancelling_timers.clear()
         return has_new_timer
 
-    def check_and_execute(self):
+    def check_and_execute(self) -> float:
         """Get expired timers and execute callbacks for the timers.
 
-        :returns: duration of next expired timer.
-        :rtype: ``float``
+        Returns:
+            Duration of next expired timer.
         """
 
         (next_expired_time, expired_timers) = self.get_expired_timers()
@@ -208,9 +209,9 @@ class TimerQueue:
                                                                \|/
     Threading/Process Pool <---- TaskQueue <--enqueue-- Timers' callback (nonblocking)
 
-    Usage::
-           >>> from solnlib import time_queue
-           >>> tq = time_queue.TimerQueue()
+    Examples:
+           >>> from solnlib import timer_queue
+           >>> tq = timer_queue.TimerQueue()
            >>> tq.start()
            >>> t = tq.add_timer(my_func, time.time(), 10)
            >>> # do other stuff
@@ -245,20 +246,21 @@ class TimerQueue:
         self._wakeup(TEARDOWN_SENTINEL)
         self._thr.join()
 
-    def add_timer(self, callback, when, interval, ident=None):
+    def add_timer(
+        self, callback: Callable, when: int, interval: int, ident: int = None
+    ) -> Timer:
         """Add timer to the queue.
 
-        :param callback: Arbitrary callable object.
-        :type callback: ``callable object``
-        :param when: The first expiration time, seconds since epoch.
-        :type when: ``integer``
-        :param interval: Timer interval, if equals 0, one time timer, otherwise
-            the timer will be periodically executed
-        :type interval: ``integer``
-        :param ident: (optional) Timer identity.
-        :type ident:  ``integer``
-        :returns: A timer object which should not be manipulated directly by
-            clients. Used to delete/update the timer
+        Arguments:
+            callback: Arbitrary callable object.
+            when: The first expiration time, seconds since epoch.
+            interval: Timer interval, if equals 0, one time timer, otherwise
+                the timer will be periodically executed
+            ident: (optional) Timer identity.
+
+        Returns:
+            A timer object which should not be manipulated directly by
+                clients. Used to delete/update the timer.
         """
 
         with self._lock:
@@ -266,11 +268,11 @@ class TimerQueue:
         self._wakeup()
         return timer
 
-    def remove_timer(self, timer):
+    def remove_timer(self, timer: Timer):
         """Remove timer from the queue.
 
-        :param timer: Timer object which is returned by ``TimerQueue.add_timer``.
-        :type timer: ``Timer``
+        Arguments:
+            timer: Timer object to remove.
         """
 
         with self._lock:
