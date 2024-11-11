3ce245038b7957219ed315d5b9839a4f7f02a1ed
hetangmodi-crest <hemodi@splunk.com>
2024-11-11 21:17:53 +0530

    test: add smoke and unit test case for proxy fetching

--- tests/unit/test_conf_manager.py
+++ tests/unit/test_conf_manager.py
@@ -16,12 +16,19 @@
 from unittest import mock
 
 from solnlib import conf_manager
+from solnlib.soln_exceptions import (
+    InvalidHostnameError,
+    InvalidPortError,
+    ConfManagerException,
+    ConfStanzaNotExistException,
+)
+import pytest
 
 
 @mock.patch.object(conf_manager, "ConfManager")
 def test_get_log_level_when_error_getting_conf(mock_conf_manager_class):
     mock_conf_manager = mock_conf_manager_class.return_value
-    mock_conf_manager.get_conf.side_effect = conf_manager.ConfManagerException
+    mock_conf_manager.get_conf.side_effect = ConfManagerException
     expected_log_level = "INFO"
 
     log_level = conf_manager.get_log_level(
@@ -57,7 +64,7 @@ def test_get_log_level_with_no_logging_stanza(mock_conf_manager_class):
     mock_conf_manager = mock_conf_manager_class.return_value
     mock_conf_manager.get_conf.return_value = mock.MagicMock()
     mock_conf_manager.get_conf.return_value.get.side_effect = (
-        conf_manager.ConfStanzaNotExistException
+        ConfStanzaNotExistException
     )
     logger = mock.MagicMock()
     expected_log_level = "INFO"
@@ -89,3 +96,148 @@ def test_get_log_level_with_default_fields(mock_conf_manager_class):
     )
 
     assert log_level == expected_log_level
+
+
+@mock.patch.object(conf_manager, "ConfManager")
+def test_get_proxy_dict_with_default_fields(mock_conf_manager_class):
+    mock_conf_manager = mock_conf_manager_class.return_value
+    mock_conf_manager.get_conf.return_value = {
+        "proxy": {
+            "proxy_enabled": "",
+            "proxy_type": "http",
+            "proxy_url": "",
+            "proxy_port": "",
+            "proxy_username": "",
+            "proxy_password": "",
+            "proxy_rdns": "",
+        }
+    }
+
+    expected_proxy_dict = {
+        "proxy_enabled": "",
+        "proxy_type": "http",
+        "proxy_url": "",
+        "proxy_port": "",
+        "proxy_username": "",
+        "proxy_password": "",
+        "proxy_rdns": "",
+    }
+
+    proxy_dict = conf_manager.get_proxy_dict(
+        logger=mock.MagicMock(),
+        session_key="session_key",
+        app_name="app_name",
+        conf_name="conf_name",
+    )
+
+    assert proxy_dict == expected_proxy_dict
+
+
+@mock.patch.object(conf_manager, "ConfManager")
+def test_get_proxy_dict_with_custom_stanza_name(mock_conf_manager_class):
+    mock_conf_manager = mock_conf_manager_class.return_value
+    # Mock configuration with a different stanza name, e.g., "custom_stanza"
+    mock_conf_manager.get_conf.return_value = {
+        "custom_stanza": {
+            "proxy_enabled": "",
+            "proxy_type": "http",
+            "proxy_url": "",
+            "proxy_port": "",
+            "proxy_username": "",
+            "proxy_password": "",
+            "proxy_rdns": "",
+        }
+    }
+
+    expected_proxy_dict = {
+        "proxy_enabled": "",
+        "proxy_type": "http",
+        "proxy_url": "",
+        "proxy_port": "",
+        "proxy_username": "",
+        "proxy_password": "",
+        "proxy_rdns": "",
+    }
+
+    proxy_dict = conf_manager.get_proxy_dict(
+        logger=mock.MagicMock(),
+        session_key="session_key",
+        app_name="app_name",
+        conf_name="conf_name",
+        proxy_stanza="custom_stanza",  # passed different stanza name
+    )
+
+    assert proxy_dict == expected_proxy_dict
+
+
+@mock.patch.object(conf_manager, "ConfManager")
+def test_get_proxy_dict_invalid_port(mock_conf_manager_class):
+    mock_conf_manager = mock_conf_manager_class.return_value
+    # Mock return value for the "proxy" stanza
+    mock_conf_manager.get_conf.return_value = {
+        "proxy": {
+            "proxy_enabled": "",
+            "proxy_type": "http",
+            "proxy_url": "example.com",
+            "proxy_port": "invalid_port",  # Invalid port
+            "proxy_username": "",
+            "proxy_password": "",
+            "proxy_rdns": "",
+        }
+    }
+
+    with pytest.raises(InvalidPortError, match="The provided port is not valid."):
+        conf_manager.get_proxy_dict(
+            logger=mock.MagicMock(),
+            session_key="session_key",
+            app_name="app_name",
+            conf_name="conf_name",
+            proxy_port="proxy_port",  # Check for invalid port
+        )
+
+
+@mock.patch.object(conf_manager, "ConfManager")
+def test_get_proxy_dict_invalid_hostname(mock_conf_manager_class):
+    mock_conf_manager = mock_conf_manager_class.return_value
+    # Mock return value for the "proxy" stanza
+    mock_conf_manager.get_conf.return_value = {
+        "proxy": {
+            "proxy_enabled": "",
+            "proxy_type": "http",
+            "proxy_url": "invalid_host",  # Invalid hostname
+            "proxy_port": "8080",
+            "proxy_username": "",
+            "proxy_password": "",
+            "proxy_rdns": "",
+        }
+    }
+
+    with pytest.raises(
+        InvalidHostnameError, match="The provided hostname is not valid."
+    ):
+        conf_manager.get_proxy_dict(
+            logger=mock.MagicMock(),
+            session_key="session_key",
+            app_name="app_name",
+            conf_name="conf_name",
+            proxy_host="proxy_url",  # Check for invalid hostname
+        )
+
+
+@mock.patch.object(conf_manager, "ConfManager")
+def test_get_proxy_dict_conf_manager_exception(mock_conf_manager_class):
+    mock_conf_manager = mock_conf_manager_class.return_value
+    mock_conf_manager.get_conf.side_effect = ConfManagerException
+
+    logger = mock.MagicMock()
+    proxy_dict = conf_manager.get_proxy_dict(
+        logger=logger,
+        session_key="session_key",
+        app_name="app_name",
+        conf_name="conf_name",
+    )
+
+    logger.error.assert_called_once_with(
+        "Failed to fetch configuration file 'conf_name'."
+    )
+    assert proxy_dict == {}
