99523de2ac8e1754c99f7cc0275cf7ec902e354f
sgoral <sgoral@splunk.com>
2025-03-04 14:09:16 +0100

    refactor: improve information in the user-agent param

---
+++ tests/integration/test_rest_client.py
@@ -0,0 +1,49 @@
+#
+# Copyright 2025 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import context
+import pytest
+import solnlib
+import sys
+from splunklib import binding
+from solnlib import splunk_rest_client as rest_client
+
+from _search import search
+
+
+host = "localhost"
+management_port = 8089
+
+
+def test_rest_client_user_agent():
+    test_url = r'search index = _internal uri_path="*/servicesNS/nobody/test_app/some/unexisting/url"'
+    user_agent = f"solnlib/{solnlib.__version__} rest-client {sys.platform}"
+    session_key = context.get_session_key()
+    wrong_url = r"some/unexisting/url"
+    rc = rest_client.SplunkRestClient(
+        session_key,
+        app="test_app",
+        owner=context.owner,
+        scheme=context.scheme,
+        host=context.host,
+        port=context.port
+    )
+    with pytest.raises(binding.HTTPError):
+        rc.get(wrong_url)
+
+    search_results = search(session_key, test_url)
+    assert len(search_results) == 1
+    assert user_agent in search_results[0]["_raw"]
